de7836f05d1979790c7e11570704b0a88a0a1e21
jj <71215065+junnjiee@users.noreply.github.com>
2026-06-14 02:42:54 +0800

    delete 11ty blog

--- js/article-read-time.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const article = document.querySelector("article");
-  const text = article.innerText;
-  const words = text.trim().split(/\s+/).filter(Boolean);
-  const estReadTime = Math.ceil(words.length / 150);
-
-  const readTimeElement = document.getElementById("read-time");
-  readTimeElement.textContent = estReadTime;
-});
